946720bc33387ef24fa3703043fa2520e85607d5
merun7 <merungugelmann@gmail.com>
2025-03-14 10:38:50 +0100

    Starting with data preparation so theres time element included

--- PreliminaryAnalysis.py
+++ PreliminaryAnalysis.py
@@ -1,5 +1,6 @@
 from pandas import read_csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
 Weather = read_csv("Data/Zurich(1990-2024).csv")
 Weather["date"] = pd.to_datetime(Weather["date"]).dt.date
@@ -8,3 +9,19 @@ Weather = Weather.set_index("date")
 pd.set_option('display.max_columns', None)
 print(Weather.info)
 print(Weather.describe)
+
+Weather[['apparent_temperature_mean', 'wind_speed_10m_max', 'precipitation_hours']].plot(
+    figsize=(12, 6),
+    subplots=True,
+    title=['Apparent Temperature (°C)', 'Max Wind Speed (m/s)', 'Precipitation Hours']
+)
+
+plt.show()
+
+print(type(Weather.index))
+Weather.index = pd.to_datetime(Weather.index)
+print(type(Weather.index))
+
+weather_monthly = Weather.resample('ME').mean()
+
+print(weather_monthly.head())
