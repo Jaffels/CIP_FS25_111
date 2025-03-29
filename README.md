0d6453504c3549936e7f985cb6d6c22144d58a5f
Jaffels <jaffels@gmail.com>
2025-03-29 18:01:04 +0100

    Completed handling missing values

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
 # CIP_FS25_111
 Nhat, Merun and Johan's CIP project
 
-## Please install the following packages
+## Required packages
 pip install openmeteo-requests  
-pip install requests-cache retry-requests numpy pandas os
+requests-cache 
+retry-requests 
+numpy 
+pandas 
+os
+matplotlib
+random
+seaborn
 
 ## PLEASE RUN get_data.py FIRST
-Please run get_data.py first as this will call the api and create the 
-csv files that will be used throughout this exercise, as well as create
-the starting dataframe. We do this to limit the amount of calls to the 
-data provider to avoid spamming them with calls.
+Please run get_data.py first as this will call the api and create 
+the csv files that will be used throughout this exercise. We do 
+this to limit the amount of calls to the data provider to avoid 
+spamming them with calls.
 
 It will take several minutes to create the final csv
 
-The load the csv file called start.csv in the Data folder to start the 
-rest of the project.
+The load the csv file called start.csv in the Data folder to 
+start the rest of the project.
+
+## Column descriptions
+A description of every column is contained in the
+Field_Descriptions.html file. Open the file in a webbrowser
+to view.
 
 ## GOOGLE DOCS DOCUMENTATION
 https://docs.google.com/document/d/1iOYDg7rNV7LiT3CZ5af6nyvBacevE4x4M34tKkmadKM/edit?usp=sharing
--- get_data.py
+++ get_data.py
@@ -1,39 +1,47 @@
-#%%
+# Load all the required packages
+import pandas as pd
 import openmeteo_requests
 import requests_cache
-import pandas as pd
 from retry_requests import retry
 import os
 import time
-#%%
+
+# create a data directory call Data for our data
+os.makedirs("Data", exist_ok=True)
+print(f"Folder Data created successfully!")
+
+# Declare the latitude and longitude for both cities
 cities = {"Zurich": {'loc': ["47.3769", "8.5417"]},
 		  "Seattle": {'loc': ["47.6062", "122.3321"]}}
-#%%
+
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
-#%%
-def call_API(city, openmeteo):
-	# Set the API call parameters
-	url = "https://archive-api.open-meteo.com/v1/archive"
-	params = {
-		"latitude": cities[city]['loc'][0],
-		"longitude": cities[city]['loc'][1],
-		"start_date": "1940-01-01",
-		"end_date": "2024-12-31",
-		"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "temperature_2m_mean", "apparent_temperature_max", "apparent_temperature_min", "apparent_temperature_mean", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum", "et0_fao_evapotranspiration"],
-		"timezone": "Europe/Berlin"
-	}
-	responses = openmeteo.weather_api(url, params=params)
-	
-	# Print the first part of the API response to validate the call data is valid
-	response = responses[0]
-	print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-	print(f"Elevation {response.Elevation()} m asl")
-	print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-	print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-	
+
+def call_API(city, openmeteo, cities):
+    # Set the API call parameters
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+       "latitude": cities[city]['loc'][0],
+       "longitude": cities[city]['loc'][1],
+       "start_date": "1940-01-01",
+       "end_date": "2024-12-31",
+       "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "temperature_2m_mean", "apparent_temperature_max", "apparent_temperature_min", "apparent_temperature_mean", "sunrise", "sunset", "daylight_duration", "sunshine_duration", "precipitation_sum", "rain_sum", "snowfall_sum", "precipitation_hours", "wind_speed_10m_max", "wind_gusts_10m_max", "wind_direction_10m_dominant", "shortwave_radiation_sum", "et0_fao_evapotranspiration"],
+       "timezone": cities[city].get('timezone', "Europe/Berlin")  # Use city timezone or default to Europe/Berlin
+    }
+    responses = openmeteo.weather_api(url, params=params)
+    
+    # Print the first part of the API response to validate the call data is valid
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    
+    return response
+
+def create_dataframe(response):
 	# Process data and validate variable types
 	daily = response.Daily()
 	daily_weather_code = daily.Variables(0).ValuesAsNumpy()
@@ -63,7 +71,8 @@ def call_API(city, openmeteo):
 		end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
 		freq = pd.Timedelta(seconds = daily.Interval()),
 		inclusive = "left"
-		)}
+		).strftime('%Y/%m/%d')
+	}
 	
 	# Create the pandas dataframe
 	daily_data["weather_code"] = daily_weather_code
@@ -91,17 +100,11 @@ def call_API(city, openmeteo):
 	daily_dataframe = pd.DataFrame(data = daily_data)
 	
 	return daily_dataframe
-#%%
-def create_folder():
-	# create a data directory call Data
-	os.makedirs("Data", exist_ok=True)
-	print(f"Folder Data created successfully!")
-#%%
 def save_df(data, city):
 	# Save the dataframe as csv file
 	data.to_csv(f"Data/{city}.csv", index = False)
 	print(f"Saved data for '{city}' successfully!")
-#%%
+
 def combine_dataframes():
 	# Create a single dataframe from the 2 datasets
 	# Load the 2 datasets
@@ -109,29 +112,41 @@ def combine_dataframes():
 	df2 = pd.read_csv("Data/Seattle.csv")
 	
 	# Add a column to contains the city name
-	df1['City'] = 'Zurich'
-	df2['City'] = 'Seattle'
+	df1['city'] = 'Zurich'
+	df2['city'] = 'Seattle'
 	
 	# Concatenate the 2 dataframes
 	start_df = pd.concat([df1, df2], ignore_index=True)
 	
 	# Write combined dataframes to csv file
 	start_df.to_csv("Data/start.csv", index = False)
-#%%
-if __name__ == "__main__":
-	create_folder()
-	count = 0
-	# Call the API and save the as csv files
-	# Saving as csv files to limit the amount of calls to data provider
-	for city in cities:
-		data = call_API(city, openmeteo)
-		save_df(data, city)
 
-		# Delay the 2nd api call for 2 minutes to not violate limits
-		# on api calls
-		if count < 1:
-			time.sleep(120)
-		count += 1
+# Call the API and save the as csv files
+# Saving as csv files to limit the amount of calls to data provider
+count = 0
+for city in cities:
+	response = call_API(city, openmeteo, cities)
+	data = create_dataframe(response)
+	save_df(data, city)
+	print('\n')
 
-	combine_dataframes()
-#%%
+	# Delay the 2nd API call for 2 minutes to not violate limits on API calls
+	if count < 1:
+		time.sleep(120)
+	count += 1
+
+# Create a single dataframe from the 2 datasets
+# Load the 2 datasets
+df1 = pd.read_csv("Data/Zurich.csv")
+df2 = pd.read_csv("Data/Seattle.csv")
+
+# Add a column to contains the city name
+df1['city'] = 'Zurich'
+df2['city'] = 'Seattle'
+
+# Concatenate the 2 dataframes
+df = pd.concat([df1, df2], ignore_index=True)
+
+# Delete files no longer required
+os.remove("Data/Seattle.csv")
+os.remove("Data/Zurich.csv")
