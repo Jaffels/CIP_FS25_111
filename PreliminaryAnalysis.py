31574ae4468d61ec11a8833e2f8c0795e002ef02
merun7 <merungugelmann@gmail.com>
2025-04-04 09:42:24 +0200

    N ew Plots

--- PreliminaryAnalysis.py
+++ PreliminaryAnalysis.py
@@ -2,8 +2,9 @@ from pandas import read_csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-Weather["date"] = pd.to_datetime(Weather["date"]).dt.date
+
 Weather = read_csv("Data/Zurich(1990-2024).csv")
+Weather["date"] = pd.to_datetime(Weather["date"]).dt.date
 Weather = Weather.set_index("date")
 
 pd.set_option('display.max_columns', None)
